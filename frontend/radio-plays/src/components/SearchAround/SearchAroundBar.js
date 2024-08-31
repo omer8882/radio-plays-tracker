f11a7df3edd5b7bda24bcf895b726ffa0e6120b6
omer8882 <omer8882@gmail.com>
2024-08-31 18:09:16 +0300

    SearchAround: - Support narrow screen view - Contained in a nice looking elevated box - Right-to-left support

--- frontend/radio-plays/src/components/SearchAround/SearchAroundBar.js
+++ frontend/radio-plays/src/components/SearchAround/SearchAroundBar.js
@@ -31,57 +31,60 @@ const SearchAroundBar = ({ onSearch }) => {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Box display="flex" alignItems="center" gap={2} margin='20px 2px 10px 2px'>
-      <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSearch} >
-          חיפוש
-        </Button>
+      <Box display="flex" flexWrap="wrap" justifyContent="center" dir="rtl" alignItems="center" gap={2} 
+       style={{backgroundColor: "#F5F5F5", borderRadius: '10px', padding:'12px', margin: '10px 2px 5px 2px', width: '100%', boxSizing: 'border-box'}} sx={{ boxShadow: 2 }}>
+          <Typography variant="h6">מה הושמע ב</Typography>
 
-        <TextField
-          select
-          label="תחנה"
-          value={selectedStation}
-          onChange={(event) => setSelectedStation(event.target.value)}
-          variant="outlined"
-          size="small"
-          sx={{ width: 'auto', minWidth: 150 }}
-        >
-          {Object.entries(stations).map(([key, value]) => (
-            <MenuItem key={key} value={value}>
-              {key}
-            </MenuItem>
-          ))}
-        </TextField>
+          <DatePicker
+            label="יום"
+            value={selectedDate}
+            dir="rtl"
+            onChange={(newValue) => setSelectedDate(newValue)}
+            sx={{ width: 150, minWidth: 150 }}
+            renderInput={(params) => <TextField {...params} variant="outlined" size="small" 
+            sx={{ width: 50 }}
+            />}
+          />
 
-        <Typography variant="h6"> בתחנה </Typography>
-
-        <TimeField
-          label="שעה"
-          value={selectedTime}
-          format="HH:mm"
-          onChange={(newValue) => setSelectedTime(newValue)}
-          sx={{ width: 80, minWidth: 80 }}
-          renderInput={(params) => 
-            <TextField  {...params} variant="outlined" size="small"/>}
-        />
-        
-
-        <Typography variant="h6"> בסביבות </Typography>
+          <Typography variant="h6"> בסביבות </Typography>
 
 
-        <DatePicker
-          label="יום"
-          value={selectedDate}
-          onChange={(newValue) => setSelectedDate(newValue)}
-          sx={{ width: 150, minWidth: 150 }}
-          renderInput={(params) => <TextField {...params} variant="outlined" size="small" 
-          sx={{ width: 50 }}
-           />}
-        />
-        
-        <Typography variant="h6">מה הושמע ב</Typography>
+          <TimeField
+            label="שעה"
+            value={selectedTime}
+            format="HH:mm"
+            dir="rtl"
+            onChange={(newValue) => setSelectedTime(newValue)}
+            sx={{ width: 80, minWidth: 80 }}
+            renderInput={(params) => 
+              <TextField  {...params} variant="outlined" size="small"/>}
+          />
+
+          <Typography variant="h6"> בתחנה </Typography>
+
+          <TextField
+            select
+            label="תחנה"
+            InputLabelProps={{alignItems: "right"}}
+            value={selectedStation}
+            onChange={(event) => setSelectedStation(event.target.value)}
+            variant="outlined"
+            size="small"
+            sx={{ width: 'auto', minWidth: 150 }}
+            >
+            {Object.entries(stations).map(([key, value]) => (
+              <MenuItem key={key} value={value}>
+                {key}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSearch}>
+            חיפוש
+          </Button>
       </Box>
     </LocalizationProvider>
   );
