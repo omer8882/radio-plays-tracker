aa6ae048e6b95bc26d265451f1f144296fd49574
omer8882 <omer8882@gmail.com>
2024-09-01 00:47:53 +0300

    Last Plays as ToggleBox Implementation

--- frontend/radio-plays/src/components/SongList.js
+++ frontend/radio-plays/src/components/SongList.js
@@ -3,14 +3,9 @@ import { List, Box } from '@mui/material';
 import SongListItem from './SongListItem';
 import axios from 'axios';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import './SongList.css'; // Ensure to add relevant CSS
+import './SongList.css';
 
 const SongList = ({ station }) => {
-  /*const sim_songs = [
-                      {time: "11:11", title: "אדי באדי", artist: "שחר טבוך, אדם בוחבוט"},
-                      {time: "11:09", title: "Von Dutch", artist: "Charli XCX"},
-                      {time: "11:05", title: "בלב מדבר", artist: "מאיר אריאל"}
-  ] ;*/
   const ghost_data = [
     {time: " ", title: " ", artist: " "},
     {time: " ", title: " ", artist: " "},
@@ -18,32 +13,21 @@ const SongList = ({ station }) => {
     {time: " ", title: " ", artist: " "},
     {time: " ", title: " ", artist: " "},
     {time: " ", title: " ", artist: " "},
-    {time: " ", title: " ", artist: " "}
+    {time: " ", title: " ", artist: " "},
+    {time: " ", title: " ", artist: " "},
+    {time: " ", title: " ", artist: " "},
+    {time: " ", title: " ", artist: " "},
   ] ;
   const error_songs = [
     {time: "00:00", title: "server error", artist: ""}
   ] ;
   
-  const [songs, setSongs] = useState([]);
   const [displayedSongs, setDisplayedData] = useState(ghost_data);
-  //const [loading, setLoading] = useState(true);
 
 
   const fetchData = async () => {
-    //setLoading(true);
-    setDisplayedData(songs == [] ? ghost_data : songs);
     try {
-      const response = await axios.get(`https://server.mahushma.com/api/station_last_plays?station=${station}`);
-      const newSongs = response.data;
-      const existingSongIds = new Set(displayedSongs.map(song => song.id));
-      newSongs.forEach(song => {
-        if (!existingSongIds.has(song.id)) {
-            displayedSongs.pop();
-            displayedSongs.unshift(song);
-            existingSongIds.add(song.id);
-        }
-      });
-
+      const response = await axios.get(`https://server.mahushma.com/api/station_last_plays?station=${station.name}`);
       setDisplayedData(response.data)
     } catch (err) {
       console.error("Error fetching songs:", err);
@@ -55,12 +39,24 @@ const SongList = ({ station }) => {
 
   useEffect(() => {
     fetchData();
-    const interval = setInterval(fetchData, 60000); // Refresh data every 60 seconds
+    const interval = setInterval(fetchData, 2*60*1000); // Refresh data every 60 seconds
     return () => clearInterval(interval); // Cleanup interval on component unmount
   }, [station]);
 
   return (
-    <Box>
+    <Box  alignItems="center" 
+          sx={{backgroundColor: station.bgColor, 
+            justifyContent: 'center', 
+            borderRadius: '0px 0px 10px 10px', 
+            margin: '0px 2px 5px 2px', 
+            padding: '0px',
+            width: '100%',
+            boxShadow: 2,
+            boxSizing: 'border-box',
+            border: '1px solid',
+            borderTop: '0px',
+            borderColor: '#c0c0c0',
+            }}>
       <List>
         <TransitionGroup component={null}>
           {displayedSongs.map((song, index) => (
