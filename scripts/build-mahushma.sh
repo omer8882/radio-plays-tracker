472c5c2b7afb98072107fa5b92e521915e72cab4
omer8882 <omer8882@gmail.com>
2025-01-07 01:51:06 +0200

    change verbosity

--- scripts/build-mahushma.sh
+++ scripts/build-mahushma.sh
@@ -1,26 +1,26 @@
 # build-mahushma.sh
 cd "$(dirname "$0")"
 
-echo "Building Recognizer..."
+echo "[ --- Building Recognizer --- ]"
 ./build/build-recognizer.sh
 if [ $? -ne 0 ]; then
   echo "ERROR: build-recognizer failed!"
   exit 1
 fi
 
-echo "Building Server..."
+echo "[ --- Building Server --- ]"
 ./build/build-server.sh
 if [ $? -ne 0 ]; then
   echo "ERROR: build-server failed!"
   exit 1
 fi
 
-echo "Building WebUI..."
+echo "[ --- Building WebUI --- ]"
 ./build/build-webui.sh
 if [ $? -ne 0 ]; then
   echo "ERROR: build-webui failed!"
   exit 1
 fi
-
+echo
 echo "Build process complete!"
 exit 0
