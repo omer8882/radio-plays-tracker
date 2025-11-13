9657489f54614bc975fd98b40391ad985fcc5962
omer8882 <omer8882@gmail.com>
2025-11-13 03:15:06 +0200

    Add auto-apply migrations on application startup

--- backend/dotnet-server/src/RadioPlaysTracker.Api/Program.cs
+++ backend/dotnet-server/src/RadioPlaysTracker.Api/Program.cs
@@ -62,6 +62,13 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+// Auto-apply migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseCors();
 
 app.UseHttpsRedirection();
